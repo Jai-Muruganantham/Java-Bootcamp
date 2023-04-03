eb32abc506f9a04dbd9471843754a8238a3a81bd
Muruganantham Jaisankar <jaimuruganantham@gmail.com>
2023-04-03 12:01:11 +0300

    Second commit

---
+++ src/main/java/babysitting/nannyservice/NannyserviceApplication.java
@@ -0,0 +1,13 @@
+package babysitting.nannyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NannyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NannyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/babysitting/nannyservice/NannyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package babysitting.nannyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NannyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
