c7154f3b4c99f5aaea2d6a20656d045999fa73ea
Muruganantham Jaisankar <jaimuruganantham@gmail.com>
2023-04-04 00:40:02 +0300

    Basic nanny booking list(will be changed)

---
+++ src/main/resources/templates/SQLstate.sql
@@ -0,0 +1,8 @@
+use nanny2;
+CREATE TABLE nanny (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    message VARCHAR(255) NOT NULL,
+    start_Time TIMESTAMP NOT NULL,
+    end_Time TIMESTAMP NOT NULL
+);
