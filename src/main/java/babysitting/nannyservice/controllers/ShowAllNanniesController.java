46cce3e68b4fb5e7e286eac720e9740f7f5822cc
Qristina <ch.surovec@gmail.com>
2023-04-04 12:37:29 +0300

    added back method mistakenly removed from controller

--- src/main/java/babysitting/nannyservice/controllers/ShowAllNanniesController.java
+++ src/main/java/babysitting/nannyservice/controllers/ShowAllNanniesController.java
@@ -3,6 +3,7 @@ package babysitting.nannyservice.controllers;
 import babysitting.nannyservice.domain.Nanny;
 import babysitting.nannyservice.services.ShowAllNanniesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,17 +15,25 @@ import java.util.List;
 public class ShowAllNanniesController {
 
 
-        private final ShowAllNanniesService showAllNanniesService;
+    private final ShowAllNanniesService showAllNanniesService;
 
-        @Autowired
-        public ShowAllNanniesController(ShowAllNanniesService showAllNanniesService) {
-            this.showAllNanniesService = showAllNanniesService;
-        }
-
-        @GetMapping
-        public List<Nanny> getNannyMapping(){
-            return showAllNanniesService.getNannies();
-        }
+    @Autowired
+    public ShowAllNanniesController(ShowAllNanniesService showAllNanniesService) {
+        this.showAllNanniesService = showAllNanniesService;
     }
+
+    @GetMapping
+    public List<Nanny> getNannyMapping() {
+        return showAllNanniesService.getNannies();
+    }
+
+    @GetMapping("/showAllNannies")
+    public String getNannyTable (Model model) {
+        List<Nanny> nannies = showAllNanniesService.getNannies();
+        model.addAttribute("nannies", nannies);
+        return "showAllNannies";
+    }
+
+}
 
 
