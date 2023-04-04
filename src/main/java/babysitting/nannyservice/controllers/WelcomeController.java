842fab4a5e769ea056889e8641505b117c3f49cc
Qristina <ch.surovec@gmail.com>
2023-04-04 13:04:32 +0300

    trying

--- src/main/java/babysitting/nannyservice/controllers/WelcomeController.java
+++ src/main/java/babysitting/nannyservice/controllers/WelcomeController.java
@@ -3,31 +3,31 @@ package babysitting.nannyservice.controllers;
 import babysitting.nannyservice.domain.Nanny;
 import babysitting.nannyservice.services.ShowAllNanniesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "showAllNannies")
-public class ShowAllNanniesController {
-
+@Controller
+public class WelcomeController {
 
     private final ShowAllNanniesService showAllNanniesService;
 
     @Autowired
-    public ShowAllNanniesController(ShowAllNanniesService showAllNanniesService) {
+    public WelcomeController(ShowAllNanniesService showAllNanniesService) {
         this.showAllNanniesService = showAllNanniesService;
     }
-
-    @GetMapping
-    public List<Nanny> getNannyMapping() {
-        return showAllNanniesService.getNannies();
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
     }
 
+    @GetMapping("/nanniesTable")
+    public String getsShowALlNannies(Model model) {
+        List<Nanny> nannies = showAllNanniesService.getNannies();
+        model.addAttribute("nannies", nannies);
+        return "nanniesTable";
+    }
 
 }
-
-
